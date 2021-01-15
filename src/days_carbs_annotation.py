d3fa22f9d3e78ccd14079f33830d611574906dbe
AQ <anna.quinlan123@gmail.com>
2021-01-14 18:45:17 -0600

    Improve export names for days carb annotation

--- src/days_carbs_annotation.py
+++ src/days_carbs_annotation.py
@@ -14,19 +14,13 @@ issue_report_date = "issue_report_date"
 short_file_name = "carb_annotated"
 days_carbs = "days_carb_data"
 
+num_with_less_carb_data = 0
+
 
 def export(dataframe, df_descriptor):
     dataframe.to_csv(
         utils.get_save_path_with_file(
-            short_file_name,
-            analysis_name,
-            df_descriptor
-            + "_"
-            + short_file_name
-            + "_"
-            + utils.get_file_stamps()[0]
-            + ".csv",
-            "dataset-creation",
+            short_file_name, analysis_name, df_descriptor + ".csv", "dataset-creation",
         )
     )
 
@@ -40,8 +34,11 @@ for i, file_path in enumerate(all_patient_files):
     )
     if days_with_carb_data < 14:
         print("! {} days w/carbs".format(days_with_carb_data))
+        num_with_less_carb_data += 1
     df[days_carbs] = days_with_carb_data
 
+    print(file_path, file_path.split("/")[-1], file_path.split("/")[-1].split(".")[0])
     file_name = file_path.split("/")[-1].split(".")[0]
     export(df, file_name)
 
+print("Found {} files with < 14 days carb data".format(num_with_less_carb_data))
