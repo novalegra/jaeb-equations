9b71d35655f22e43d348098ff4cebd73fd8de4b2
AQ <anna.quinlan123@gmail.com>
2021-02-15 14:54:54 -0600

    Make it possible to grab distributions for multiple files at a time

--- src/get_distributions.py
+++ src/get_distributions.py
@@ -1,45 +1,76 @@
 import pandas as pd
 import utils
 
-file_name = "processed-30-min-win_all_selected_rows_2021_02_14_23-v0_1-b8c3222"
-data_path = utils.find_full_path(file_name, ".csv")
-df = pd.read_csv(data_path)
-
 distribution_stats = ["mean", "std", "min", "25%", "75%", "max"]
-output_df = pd.DataFrame(columns=distribution_stats)
 analysis_name = "analyze-distributions"
 
-# Keys for working with exports
+# Keys for non-metabolic settings
 suspend_threshold = "suspend_threshold"
 correction_lower = "correction_target_lower"
 correction_upper = "correction_target_upper"
 max_basal = "maximum_basal_rate"
 max_bolus = "maximum_bolus"
 
-keys = [suspend_threshold, correction_lower, correction_upper, max_basal, max_bolus]
+# Keys for settings
+bmi = "BMI"
+bmi_percentile = "BMIPercentile"
+age = "Age"
+tdd = "avg_total_insulin_per_day_outcomes"
+basal = "total_daily_scheduled_basal"  # Total daily basal
+total_daily_carbs = "avg_carbs_per_day_outcomes"  # Total daily CHO
+isf = "avg_isf"
+icr = "weighted_cir_outcomes"
 
-# Filter out unreasonable settings
-df = df[
-    (df[suspend_threshold] > 10)
-    & (df[correction_lower] > 10)
-    & (df[correction_upper] > 10)
+
+file_names = [
+    "test_1_overall_aspirational_2021_02_04_22-v0_1-4d1a82f",
+    "test_2_overall_aspirational_2021_02_04_22-v0_1-4d1a82f",
+    "test_3_overall_aspirational_2021_02_04_22-v0_1-4d1a82f",
+    "test_4_overall_aspirational_2021_02_04_22-v0_1-4d1a82f",
+    "test_5_overall_aspirational_2021_02_04_22-v0_1-4d1a82f",
+    "train_1_overall_aspirational_2021_02_04_22-v0_1-4d1a82f",
+    "train_2_overall_aspirational_2021_02_04_22-v0_1-4d1a82f",
+    "train_3_overall_aspirational_2021_02_04_22-v0_1-4d1a82f",
+    "train_4_overall_aspirational_2021_02_04_22-v0_1-4d1a82f",
+    "train_5_overall_aspirational_2021_02_04_22-v0_1-4d1a82f",
 ]
 
-""" Print out distributions for all the different columns """
-for key in keys:
-    row = []
-    distribution = df[key].describe(include="all")
-    for stat in distribution_stats:
-        row.append(distribution.loc[stat])
 
-    if len(row) == len(distribution_stats):
-        output_df.loc[key] = row
+def get_and_export_settings(df, file_name):
+    keys = [bmi, bmi_percentile, age, tdd, basal, total_daily_carbs, isf, icr]
 
-output_df.to_csv(
-    utils.get_save_path_with_file(
-        file_name,
-        analysis_name,
-        "distributions_" + utils.get_file_stamps()[0] + ".csv",
-        "data-processing",
+    # keys = [suspend_threshold, correction_lower, correction_upper, max_basal, max_bolus]
+
+    # Filter out unreasonable settings
+    # df = df[
+    #     (df[suspend_threshold] > 10)
+    #     & (df[correction_lower] > 10)
+    #     & (df[correction_upper] > 10)
+    # ]
+
+    output_df = pd.DataFrame(columns=distribution_stats)
+
+    """ Print out distributions for all the different columns """
+    for key in keys:
+        row = []
+        distribution = df[key].describe(include="all")
+        for stat in distribution_stats:
+            row.append(distribution.loc[stat])
+
+        if len(row) == len(distribution_stats):
+            output_df.loc[key] = row
+
+    output_df.to_csv(
+        utils.get_save_path_with_file(
+            file_name,
+            analysis_name,
+            file_name[0:15] + "_distributions_" + utils.get_file_stamps()[0] + ".csv",
+            "data-processing",
+        )
     )
-)
+
+
+for f_name in file_names:
+    data_path = utils.find_full_path(f_name, ".csv")
+    df = pd.read_csv(data_path)
+    get_and_export_settings(df, f_name)
