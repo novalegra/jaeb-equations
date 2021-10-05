33e4b047d976abc0391e86fb5932d47d6d11f6c1
AQ <anna.quinlan123@gmail.com>
2021-10-05 09:08:13 -0700

    update graphs to use y_col_name

--- src/parameter_graphs.py
+++ src/parameter_graphs.py
@@ -13,18 +13,21 @@ def get_param_combos():
     return product(BMI_PARAMS, CARB_PARAMS)
 
 
-def make_graphs(equation, parameters_to_estimate, X_col_names):
+def make_graphs(equation, fixed_parameters, X_col_names, y_col_name):
     """
     equation: equation to use to generate plots
     parameters_to_estimate: coefficients of parameters in equation
     X_col_names: names of parameters in equation, in order of input
+    y_col_name: name of y column
     """
     if not (
         ("BMI" in X_col_names or "log_BMI" in X_col_names)
         and ("CHO" in X_col_names or "log_CHO" in X_col_names)
         and ("TDD" in X_col_names or "log_TDD" in X_col_names)
     ):
-        print("ERROR: unable to run because BMI, CHO, or TDD is missing as a parameter")
+        print(
+            "ERROR: unable to create graphs because BMI, CHO, or TDD is missing as a parameter"
+        )
         return
 
     fig, axs = plt.subplots(3, 3)
@@ -33,7 +36,7 @@ def make_graphs(equation, parameters_to_estimate, X_col_names):
     max_y, min_y = -float("inf"), float("inf")
 
     for i, combo in enumerate(get_param_combos()):
-        x, y = get_x_y(combo, equation, parameters_to_estimate, X_col_names)
+        x, y = get_x_y(combo, equation, fixed_parameters, X_col_names, y_col_name)
         max_y = max(max_y, max(y))
         min_y = min(min_y, min(y))
         cache.append((x, y, i, combo))
@@ -52,7 +55,7 @@ def make_graphs(equation, parameters_to_estimate, X_col_names):
     plt.show()
 
 
-def get_x_y(combo, equation, parameters_to_estimate, X_col_names):
+def get_x_y(combo, equation, fixed_parameters, X_col_names, y_col_name):
     bmi, cho = combo
     param_dict = {
         "BMI": bmi,
@@ -69,13 +72,15 @@ def get_x_y(combo, equation, parameters_to_estimate, X_col_names):
         param_dict["TDD"] = tdd
         param_dict["log_TDD"] = np.log(tdd + LOG_CONSTANT)
 
-        fixed_parameters = [param_dict[param] for param in X_col_names]
+        X_val = [param_dict[param] for param in X_col_names]
 
-        y_pred = equation(parameters_to_estimate, fixed_parameters)
-        if y_pred is None:
-            print("CAUTION!! y_pred is none")
+        y_predict = equation(fixed_parameters, X_val)
+        if y_predict is None:
+            print("CAUTION!! y_predict is None")
+        elif "log" in y_col_name:
+            y_predict = np.exp(y_predict)
 
-        y_preds.append(y_pred)
+        y_preds.append(y_predict)
 
     return tdd_range, y_preds
 
@@ -96,6 +101,6 @@ def linear_regression_equation(
 
 
 # Example call
-# make_graphs(
-#     linear_regression_equation, [0.15, 0.5, 0.35], ["log_CHO", "log_TDD", "BMI"]
-# )
+make_graphs(
+    linear_regression_equation, [0.15, 0.5, 0.35], ["log_CHO", "log_TDD", "BMI"], "CARB"
+)
